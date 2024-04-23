06a1890d8f121865cf21474c4e18b4fa936365af
Steffany Livino <steffanylivino02@gmail.com>
2024-04-22 22:53:29 -0300

    atualizações

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,17 @@
 <template>
   <v-app>
     <v-container>
-                <v-row class="title" style="text-align:center; ">
-            <v-col cols="6">
-              <h1 style="font-weight: 300; font-size: 4rem">Pokedex</h1> 
-          </v-col>
+      <v-row class="title" style="text-align:center;">
+        <v-col cols="12">
+          <h1 style="font-weight: 300; font-size: 4rem">Pokedex</h1> 
+        </v-col>
       </v-row>
       <v-container class="pokemon-container">
         <v-text-field v-model="search" label="Pesquisar" placeholder="Pesquisar Pokémon" solo></v-text-field>
         <v-select v-model="searchType" :items="searchOptions" label="Tipo de Pesquisa" style="width: 10%;"></v-select>
         <v-row class="row">
           <v-col cols="3" v-for="pokemon in filtered_pokemon" :key="pokemon.name">
-            <v-card @click="show_pokemon(pokemon)" class="pokemon-card" :flat ="true">
+            <v-card @click="show_pokemon(pokemon)" class="pokemon-card" :flat="true">
               <v-container>
                 <v-row class="mx-0 d-flex justify-center">
                   <div class="pokemon-image-container">
@@ -29,11 +29,10 @@
     <v-dialog v-model="dialog" width="500">
       <template v-slot:activator="{ on }">
         <v-btn color="red lighten-2" dark v-on="on">
-          Click Me
         </v-btn>
       </template>
-      <v-card>
-        <v-card-text v-if="selected_pokemon">
+      <v-card v-if="selected_pokemon">
+        <v-card-text>
           <v-container class="detail">
             <img :src="getImageUrl(selected_pokemon)" :alt="selected_pokemon.name" class="pokemon-image-id">
             <h2 class="mx-0 d-flex justify-center">{{ selected_pokemon.name }}</h2>
@@ -46,33 +45,33 @@
               <div class="right">{{ selected_pokemon.id }}</div>
             </div>
             <div class="property">
-              <div class="left">Height</div>
+              <div class="left">Altura</div>
               <div class="right">{{ selected_pokemon.height / 10 }} m</div>
             </div>
             <div class="property">
               <div class="left">Peso</div>
               <div class="right">{{ selected_pokemon.weight }} kg</div>
             </div>
-            <h3>Pokemon Tipos</h3>
+            <h3>Pokemon Tipo</h3>
             <div class="types">
-              <div class="type" v-for="(value, index) in selected_pokemon.types" :key="'value'+index">
-                {{ value.type.name }}
+              <div class="type" v-for="(type, index) in selected_pokemon.types" :key="index">
+                {{ type.type.name }}
               </div>
             </div>
-            <h3>Habilidades</h3>
-            <div class="abilities">
-              <div class="ability" v-for="(value, index) in selected_pokemon.abilities" :key="'value'+index">
-                {{ value.ability.name }}
+            <h3>Habilidade</h3>
+            <div class="habilities">
+              <div class="hability" v-for="(ability, index) in selected_pokemon.abilities" :key="index">
+                {{ ability.ability.name }}
               </div>
             </div>
-            <h3>Evolução</h3>
+            <h3>Evoluções</h3>
             <div>
               <v-row class="mx-0 d-flex align-center">
                 <template v-for="(evolution_detail, index) in evolutions">
                   <v-col cols="12" md="3" :key="`evolution-${index}`">
-                    <v-card v-if="evolution_detail.imageUrl" :flat ="true" class="evolution-card">
+                    <v-card v-if="evolution_detail.imageUrl" :flat="true" class="evolution-card">
                       <v-container>
-                        <img :src="evolution_detail.imageUrl" :alt="evolution_detail.name"  class="evolution-card">
+                        <img :src="evolution_detail.imageUrl" :alt="evolution_detail.name" class="evolution-card">
                         <h2 class="mx-0 d-flex justify-center">{{ evolution_detail.name }}</h2>
                       </v-container>
                     </v-card>
@@ -83,21 +82,34 @@
                 </template>
               </v-row>
             </div>
-            <h3>Indices de Jogos</h3>
-            <div class="game-indices">
-    
-   
-    <div class="ability" v-for="(gameIndex, index) in games_indices" :key="'gameIndex'+index">
-          
-          
-    <div>Índice do Jogo: {{ gameIndex.gameIndex }}</div>
-          
-        
-    <div>Nome do Movimento: {{ gameIndex.move.name }}</div>
-          <div>URL do Movimento: {{ gameIndex.move.url }}</div>
+            <div>
+    <div v-if="gamesIndices.length > 0">
+      <div class="card">
+        <div class="card-header">
+          <h2>Game Indices</h2>
         </div>
-      
+        <div class="card-body">
+          <table class="table">
+            <thead>
+              <tr>
+                <th>Versão</th>
+                <th>Índice do Jogo</th>
+              </tr>
+            </thead>
+            <tbody>
+              <tr v-for="(gameIndex, index) in gamesIndices" :key="index">
+                <td>{{ gameIndex.versionName }}</td>
+                <td>{{ gameIndex.gameIndex }}</td>
+              </tr>
+            </tbody>
+          </table>
+        </div>
+      </div>
     </div>
+    <div v-else>
+      <p>Não há informações de jogo disponíveis.</p>
+    </div>
+  </div>
           </v-container>
         </v-card-text>
       </v-card>
@@ -115,12 +127,12 @@ export default {
       pokemons: [],
       search: "",
       searchType: "nome",
-      searchOptions: ["nome", "id","tipo","especie"],
+      searchOptions: ["nome", "id", "tipo", "espécie"],
       dialog: false,
       selected_pokemon: null,
       evolution: null,
-      evolutions: [], // Adiciona a propriedade evolutions no objeto de dados
-      games_indices: []
+      evolutions: [], 
+      gamesIndices: []
     };
   },
   mounted() {
@@ -129,8 +141,6 @@ export default {
         ...pokemon,
         id: this.getId(pokemon)
       }));
-      
-      // Verifica se há pelo menos um Pokémon antes de fazer a solicitação
       if (this.pokemons.length > 0) {
         axios.get(this.pokemons[0].url).then(response => {
           this.selected_pokemon = response.data;
@@ -151,37 +161,42 @@ export default {
     show_pokemon(pokemon) {
       axios.get(pokemon.url).then((response) => {
         this.selected_pokemon = response.data;
-        // Obtém as informações de evolução e games_indices
         this.fetch_evolution();
         this.fetch_games_indices(); 
         this.dialog = true;
       });
     },
     fetch_games_indices() {
-      axios.get(this.selected_pokemon.species.url).then(response => {
-        const speciesData = response.data;
-        if (speciesData.game_indices) {
-          const promises = speciesData.game_indices.map(gameIndex => {
-            return axios.get(gameIndex.version.url);
-          });
-
-          Promise.all(promises).then(responses => {
-            this.games_indices = responses.map(response => {
-              return {
-                versionName: response.data.name,
-                gameIndex: speciesData.game_indices.find(gameIndex => gameIndex.version.url === response.config.url).game_index,
-                move: {
-                  name: speciesData.move.name,
-                  url: speciesData.move.url
-                }
-              };
-            });
-          });
-        } else {
-          this.games_indices = [];
-        }
+      axios.get(`https://pokeapi.co/api/v2/pokemon/${this.selected_pokemon.id}`)
+  .then(response => {
+    const pokemonData = response.data;
+    if (pokemonData.game_indices && pokemonData.game_indices.length > 0) {
+      const promises = pokemonData.game_indices.map(indexData => {
+        return axios.get(indexData.version.url);
       });
-    },
+
+      Promise.all(promises)
+        .then(responses => {
+          const gamesIndices = responses.map((response, index) => {
+            const versionName = response.data.name;
+            const gameIndex = pokemonData.game_indices[index].game_index;
+            return {
+              versionName: versionName,
+              gameIndex: gameIndex
+            };
+          });
+          this.gamesIndices = gamesIndices; // Retorna os dados do jogo para o componente
+        })
+        .catch(error => {
+          console.error('Erro ao buscar dados das versões:', error);
+        });
+    } else {
+      this.gamesIndices = []; // Se não houver informações de game_indices, define gamesIndices como vazio
+    }
+  })
+  .catch(error => {
+    console.error('Erro ao buscar dados do Pokémon:', error);
+  });},
     fetch_evolution() {
       axios.get(`https://pokeapi.co/api/v2/pokemon/${this.selected_pokemon.id}`).then((response) => { 
         const speciesName = response.data.species.name; 
@@ -191,12 +206,11 @@ export default {
             axios.get(evolutionChainUrl).then((response) => {
               const evolutionChain = response.data.chain;
               this.evolution = evolutionChain;
-              this.updateEvolutions(); // Atualiza as evoluções após obter os dados
+              this.updateEvolutions(); 
             });
-          } else {
-            // Pokémon não tem evolução
+          } else { 
             this.evolution = null;
-            this.evolutions = []; // Limpa as evoluções
+            this.evolutions = []; 
           }
         }).catch(error => {
           console.error('Erro ao buscar dados de evolução:', error);
@@ -213,13 +227,11 @@ export default {
 
       while (evolution && evolution.species) {
         if (evolution.species) {
-          // Adiciona o Pokémon da evolução com sua imagem usando o ID
           chain.push({
             name: evolution.species.name,
             imageUrl: getPokemonImageUrlById(evolution.species.url.split("/")[6])
           });
         }
-        // Verifica se há evoluções adicionais
         if (evolution.evolves_to && evolution.evolves_to.length > 0) {
           evolution = evolution.evolves_to[0];
         } else {
@@ -236,10 +248,14 @@ export default {
       switch (this.searchType) {
         case "id":
           return this.pokemons.filter(pokemon => pokemon.id.toString().includes(this.search));
-        case "tipo":
-          return this.pokemons.filter(pokemon => pokemon.type.toLowerCase().includes(this.search.toLowerCase()));
+          case "tipo":
+            return this.pokemons.filter(pokemon => 
+              pokemon.types && pokemon.types.some(type => type.type.name.toLowerCase().includes(this.search.toLowerCase()))
+            );
         case "espécie":
-          return this.pokemons.filter(pokemon => pokemon.species.toLowerCase().includes(this.search.toLowerCase()));
+          return this.pokemons.filter(pokemon => 
+            pokemon.species.toLowerCase().includes(this.search.toLowerCase())
+          );
         case "nome":
         default:
           return this.pokemons.filter(pokemon => pokemon.name.toLowerCase().includes(this.search.toLowerCase()));
@@ -321,7 +337,7 @@ export default {
   .right { float: right; }
 }
 
-.types, .abilities {
+.types, .habilities {
           display: flex;
           justify-content: flex-start;
           flex-wrap: wrap;
@@ -329,7 +345,7 @@ export default {
           max-width: 400px;
 }          
 
-.type, .ability {
+.type, .hability {
             margin: 5px 10px 10px 0;
             padding: 5px 10px;
             border-radius: 20px;
@@ -342,9 +358,43 @@ export default {
 }
 
 .type { background-color:#B296FF; }
-.ability { background-color: #C1D2DC; }
+.hability { background-color: #C1D2DC; }
 
 .evolution-card{
   margin-right: 20%;
+}
+.table {
+  width: 100%;
+  border-collapse: collapse;
+  border-radius: 5px;
+  overflow: hidden;
+  box-shadow: 0 0 20px rgba(0, 0, 0, 0.1);
+  margin-top: 20px;
+}
+
+.table th {
+  background-color: #f2f2f2;
+  color: #333;
+  font-weight: bold;
+  padding: 10px;
+  text-align: left;
+  border-bottom: 1px solid #ddd;
+}
+
+.table td {
+  padding: 10px;
+  border-bottom: 1px solid #ddd;
+}
+
+.table td:first-child {
+  font-weight: bold;
+}
+
+.table .highlight {
+  background-color: #f5f5f5;
+}
+
+.table tr:nth-child(odd) {
+  background-color: #fafafa;
 }
 </style>
